1a3c7feb2666d4f21359ed47886c573d053efbc9
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-15 09:50:25 -0300

    Evento de mover mouse e cor do balão

--- script/balaozinho.js
+++ script/balaozinho.js
@@ -5,7 +5,13 @@ const marcacoes = document.querySelectorAll('.marcacao');
 marcacoes.forEach(marcacao => {
     marcacao.addEventListener('mouseenter', () => {
         balaozinho.innerHTML = `<h2>${marcacao.dataset.titulo}</h2><p>${marcacao.dataset.conteudo}</p>`;
+        balaozinho.style.color = marcacao.dataset.cor;
         balaozinho.style.display = 'block';
+    });
+
+    marcacao.addEventListener('mousemove', (evento) => {
+        balaozinho.style.left = evento.pageX + 'px';
+        balaozinho.style.top = evento.pageY + 'px';
     });
 
     marcacao.addEventListener('mouseleave', () => {
