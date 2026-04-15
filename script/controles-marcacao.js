59db2c644ee1a96432b57e7965b218e689c3ccff
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-15 10:12:01 -0300

    preenche campos

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -15,3 +15,27 @@ function ocultaMarcacacao() {
 }
 
 document.querySelector('#visibilidade-das-marcacoes').addEventListener('change', ocultaMarcacacao);
+
+function atualizaControles(marcacaoEl) {
+    document.querySelector('#x-da-marcacao').value = parseInt(marcacaoEl.style.left);
+    document.querySelector('#y-da-marcacao').value = parseInt(marcacaoEl.style.top);
+    document.querySelector('#largura-da-marcacao').value = parseInt(marcacaoEl.style.width);
+    document.querySelector('#altura-da-marcacao').value = parseInt(marcacaoEl.style.height);
+
+    document.querySelector('#titulo-da-marcacao').value = marcacaoEl.dataset.titulo;
+    document.querySelector('#conteudo-da-marcacao').value = marcacaoEl.dataset.conteudo;
+    document.querySelector('#cor-da-marcacao').value = marcacaoEl.dataset.cor;
+
+    const formato = marcacaoEl.classList.contains('formato-oval') ? 'formato-oval' : 'formato-retangular';
+    document.querySelector(`input[value="${formato}"]`).checked = true;
+}
+
+
+
+marcacoes.forEach(marcacao => {
+    marcacao.addEventListener('click', () => {
+        document.querySelector('.marcacao.selecionada')?.classList.remove('selecionada');
+        marcacao.classList.add('selecionada');
+        atualizaControles(marcacao);
+    });
+});
